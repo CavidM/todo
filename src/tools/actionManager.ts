d277c3f0238ddf8e776a878833d2c947f6aa14e0
CavidM <cavid2409@gmail.com>
2021-01-20 21:50:37 +0400

    documenting actionManager

--- src/tools/actionManager.ts
+++ src/tools/actionManager.ts
@@ -9,10 +9,30 @@ export interface ActionMeta {
 }
 
 export interface BaseAction {
-  payload: any;
-  meta: ActionMeta;
-  finish: (status: actionStatus) => void;
+  /**
+   * Action type or Event name which triggered from any part of application by user
+   * Should hold basic important information about triggered action.
+   *  domain name, operation which perform on this domain and may hold the final status about operation
+   * Example: domain/action:status, user/add -> user/add:(success | fail)
+   */
   type: string;
+
+  /**
+   * The main data which this action (event) will trigger for.
+   */
+  payload: any;
+
+  /**
+   * Hold any information about how action was performed
+   */
+  meta: ActionMeta;
+
+  /**
+   * @param status
+   * Call this on action object which indicate that action is complete for using it in anywhere in application
+   * It can add extra data to meta property giving more  information about how action was finished
+   */
+  finish: (status: actionStatus) => void;
 }
 
 const baseAction: BaseAction = {

90a582c81a8d68ddd47e0df3096092c160fd755b
CavidM <cavid2409@gmail.com>
2021-01-29 08:24:04 +0400

    fixed webpack workbox manifest

--- service-worker.js
+++ service-worker.js
@@ -1,113 +1,23 @@
-import { CacheableResponsePlugin } from 'workbox-cacheable-response/CacheableResponsePlugin';
-import { CacheFirst, NetworkFirst } from 'workbox-strategies';
-import { ExpirationPlugin } from 'workbox-expiration/ExpirationPlugin';
-import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
-import { registerRoute } from 'workbox-routing/registerRoute';
+// import { CacheableResponsePlugin } from 'workbox-cacheable-response/CacheableResponsePlugin';
+// import { CacheFirst, NetworkFirst } from 'workbox-strategies';
+// import { ExpirationPlugin } from 'workbox-expiration/ExpirationPlugin';
+import { precacheAndRoute } from 'workbox-precaching';
+// import { registerRoute } from 'workbox-routing/registerRoute';
+import { clientsClaim } from 'workbox-core';
+
+console.log('aaa')
 
 precacheAndRoute(self.__WB_MANIFEST);
+self.addEventListener('activate', function(event) {
+  console.log('Claiming control');
+  return self.clients.claim();
+});
+self.addEventListener('install', (event) => console.log('installed'))
 
-registerRoute(
-  /^https:\/\/mylibrary\.io\/graphql\?.+cache%22:1/,
-  new NetworkFirst({
-    cacheName: 'short-cache',
-    matchOptions: {
-      ignoreVary: true
-    },
-    plugins: [
-      new ExpirationPlugin({
-        maxEntries: 500,
-        maxAgeSeconds: 300,
-        purgeOnQuotaError: true
-      }),
-      new CacheableResponsePlugin({
-        statuses: [0, 200]
-      })
-    ]
-  })
-);
 
-registerRoute(
-  /^https:\/\/mylibrary\.io\/graphql\?.+cache%22:5/,
-  new NetworkFirst({
-    cacheName: 'medium-cache',
-    matchOptions: {
-      ignoreVary: true
-    },
-    plugins: [
-      new ExpirationPlugin({
-        maxEntries: 500,
-        maxAgeSeconds: 86400,
-        purgeOnQuotaError: true
-      }),
-      new CacheableResponsePlugin({
-        statuses: [0, 200]
-      })
-    ]
-  })
-);
 
-registerRoute(
-  /^https:\/\/mylibrary\.io\/graphql\?.+cache%22:9/,
-  new NetworkFirst({
-    cacheName: 'max-cache',
-    matchOptions: {
-      ignoreVary: true
-    },
-    plugins: [
-      new ExpirationPlugin({
-        maxEntries: 500,
-        maxAgeSeconds: 63072e3,
-        purgeOnQuotaError: true
-      }),
-      new CacheableResponsePlugin({
-        statuses: [0, 200]
-      })
-    ]
-  })
-);
 
-registerRoute(
-  /^https:\/\/s3.amazonaws.com\/my-library-cover-uploads/,
-  new NetworkFirst({
-    cacheName: 'local-images1',
-    matchOptions: {
-      ignoreVary: true
-    },
-    plugins: [
-      new ExpirationPlugin({
-        maxEntries: 500,
-        maxAgeSeconds: 63072e3,
-        purgeOnQuotaError: true
-      }),
-      new CacheableResponsePlugin({
-        statuses: [0, 200]
-      })
-    ]
-  })
-);
 
-registerRoute(
-  /^https:\/\/my-library-cover-uploads.s3.amazonaws.com/,
-  new NetworkFirst({
-    cacheName: 'local-images2',
-    matchOptions: {
-      ignoreVary: true
-    },
-    plugins: [
-      new ExpirationPlugin({
-        maxEntries: 500,
-        maxAgeSeconds: 63072e3,
-        purgeOnQuotaError: true
-      }),
-      new CacheableResponsePlugin({
-        statuses: [0, 200]
-      })
-    ]
-  })
-);
-
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
+self.addEventListener('fetch',(event) => {
+  console.log('fetch');
 });
--- webpack.config.js
+++ webpack.config.js
@@ -8,13 +8,13 @@ module.exports = {
     app: {
       import: path.resolve(__dirname, 'src', 'index.tsx')
     },
-    serviceWorker: {
-      import: path.resolve(__dirname, 'service-worker.js'),
-      filename: 'service-worker.js'
-    }
+    // serviceWorker: {
+    //   import: path.resolve(__dirname, 'service-worker.js'),
+    //   filename: 'service-worker.js'
+    // }
   },
   output: {
-    path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, 'build'),
     filename: '[name].bundle.js',
     chunkFilename: '[id].[hash:8].js',
     publicPath: '/'
@@ -22,8 +22,8 @@ module.exports = {
   devServer: {
     port: 3007,
     historyApiFallback: true,
-    contentBase: path.join(__dirname, 'src/assets'),
-    contentBasePublicPath: '/assets'
+    contentBase: path.join(__dirname, 'public'),
+    contentBasePublicPath: '/public'
   },
   resolve: {
     extensions: ['*', '.js', '.jsx', '.ts', '.tsx']
@@ -50,13 +50,13 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Progressive web application',
-      template: path.resolve(__dirname, 'src/index.html'),
+      template: path.resolve(__dirname, 'public/index.html'),
       filename: 'index.html'
     }),
     new webpack.ProvidePlugin({
       React: 'react'
     }),
-    new webpack.SourceMapDevToolPlugin({})
+    new webpack.SourceMapDevToolPlugin({}),
     /*new WorkboxPlugin.GenerateSW({
       // these options encourage the ServiceWorkers to get in there fast
       // and not allow any straggling "old" SWs to hang around
@@ -64,10 +64,10 @@ module.exports = {
       skipWaiting: true,
       maximumFileSizeToCacheInBytes: 50000000
     }),*/
-    /*new WorkboxPlugin.InjectManifest({
+    new WorkboxPlugin.InjectManifest({
       swSrc: './service-worker.js',
       maximumFileSizeToCacheInBytes: 50000000
-    })*/
+    })
   ],
   optimization: {
     runtimeChunk: 'single'
